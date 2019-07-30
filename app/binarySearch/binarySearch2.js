659239ab155a6facbd7da1b38a364c20f92ea15a
jmotosp <jmotosp@gmail.com>
2019-07-30 09:48:59 +0200

    found item when it is in the lower half

--- app/binarySearch/binarySearch2.js
+++ app/binarySearch/binarySearch2.js
@@ -4,8 +4,18 @@ class BinarySearch{
     let init = 0,
         end = list.length - 1,
         med = getHalf(init, end);
-    if(list[med] === item) {
-      return med;
+    
+    while(init <= end) {
+      if(list[med] === item) {
+        return med;
+      }
+      if(med === end) {
+        return -1;
+      }
+      if (list[med] > item) {
+        end = med;
+        med = getHalf(init, end)
+      }
     }
     return -1;
   }

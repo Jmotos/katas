ad4f50bc3777c820355b4311851109b6ecc3607d
jmotosp <jmotosp@gmail.com>
2019-07-30 09:33:41 +0200

    item is founded if it is in the half of the list

--- app/binarySearch/binarySearch2.js
+++ app/binarySearch/binarySearch2.js
@@ -1,9 +1,19 @@
 class BinarySearch{
 
   chop (item, list) {
-    
+    let init = 0,
+        end = list.length - 1,
+        med = getHalf(init, end);
+    if(list[med] === item) {
+      return med;
+    }
     return -1;
   }
+}
+
+function getHalf(init, end) {
+  let half = end - init;
+  return Math.floor( half / 2 + init );
 }
 
 module.exports = BinarySearch;

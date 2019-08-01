4ce39a4b04a973eaac1161e59b8184f5aa3c8028
jmotosp <jmotosp@gmail.com>
2019-08-01 10:39:18 +0200

    find in the lower half

--- app/binarySearch/binarySearch4.js
+++ app/binarySearch/binarySearch4.js
@@ -1,9 +1,24 @@
 function BinarySearch () {
+  function getMiddlePosition(init, end) {
+    return Math.floor(( init + end ) / 2 );
+  }
   return {
     chop: function (item, list) {
-
-      if(list[0] === item) {
-        return 0;
+      let init = 0,
+          end = list.length - 1,
+          middle = getMiddlePosition(init, end);
+      while(init <= end) {
+        let foundItem = list[middle];
+        if(foundItem === item) {
+          return middle;
+        }
+        if(init === middle) {
+          return -1;
+        }
+        if(foundItem > item) {
+          end = middle;
+          middle = getMiddlePosition(init, end);
+        }
       }
       return -1;
     }

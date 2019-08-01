7139bec786bb36ba6f977f167f5e97fcee105178
jmotosp <jmotosp@gmail.com>
2019-08-01 10:46:33 +0200

    refactor to improve readability

--- app/binarySearch/binarySearch4.js
+++ app/binarySearch/binarySearch4.js
@@ -1,26 +1,42 @@
 function BinarySearch () {
+  const NOT_FOUND = -1;
+  
   function getMiddlePosition(init, end) {
     return Math.floor(( init + end ) / 2 );
   }
+  function itemNotFound(init, end) {
+    return init <= end;
+  }
+  function itemFounded(foundItem, item) {
+    return foundItem === item;
+  }
+  function itemNotExists(init, middle) {
+    return init === middle;
+  }
+  function itemNotInTheHigherHalf(foundItem, item) {
+    return foundItem > item;
+  }
+
   return {
     chop: function (item, list) {
       let init = 0,
           end = list.length - 1,
           middle = getMiddlePosition(init, end);
-      while(init <= end) {
+
+      while(itemNotFound(init, end)) {
         let foundItem = list[middle];
-        if(foundItem === item) {
+        if(itemFounded(foundItem, item)) {
           return middle;
         }
-        if(init === middle) {
-          return -1;
+        if(itemNotExists(init, middle)) {
+          return NOT_FOUND;
         }
-        if(foundItem > item) {
+        if(itemNotInTheHigherHalf(foundItem, item)) {
           end = middle;
           middle = getMiddlePosition(init, end);
         }
       }
-      return -1;
+      return NOT_FOUND;
     }
   };
 }
